cef9a02f3c556a9858c7c329b1e1e0470194f1c2
Natalya Mastyaeva <nata.m.754@gmail.com>
2023-07-12 04:03:49 +0400

    add: test_fixture7.py ref: conftest.py

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 
 @pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
+    browser.fullscreen_window()
+    browser.implicitly_wait(5)
     yield browser
     print("\nquit browser..")
     browser.quit()
